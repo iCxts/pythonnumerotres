ac3b61e4d5e3ec59213a27d9d367587178d53c30
iCxt <101859095+iCxts@users.noreply.github.com>
2026-01-19 17:53:49 +0700

    Create client.py

---
+++ simple_chat/client.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 21001
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("Connected to server")
+
+    while True:
+        message = input("Enter message: ")
+        # message = "exit"
+        s.send((message).encode())
+
+        message_received = ""
+        data = s.recv(32768)
+        if not data:
+            print("Server disconnected")
+            break
+
+        message_received += data.decode()
+
+        print("Server answer: ", message_received)
+
+    s.close()
+
+print("Client finished")
